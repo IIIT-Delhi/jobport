7a4220e9f3ec394160bc9f09ce30103fb7d44351
naman <naman13064@iiitd.ac.in>
2015-02-05 19:43:55 +0530

    admin -> batcave

--- placement/placement/urls.py
+++ placement/placement/urls.py
@@ -19,6 +19,6 @@ urlpatterns = patterns('',
 
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^batcave/', include(admin.site.urls)),
 
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
