1ac68cd1463f2fc182eb3f01cc3bc547b4029405
naman <naman13064@iiitd.ac.in>
2015-02-08 18:04:26 +0530

    codecleanup;pycharm

--- placement/jobport/helpers.py
+++ placement/jobport/helpers.py
@@ -8,82 +8,82 @@ __author__ = 'naman'
 
 
 def is_member(user, group):
-    return user.groups.filter(name=group)
+	return user.groups.filter(name=group)
 
 
 def is_eligible(candidate, job):
-    eligibility = {}
-    eligibility['value'] = True
-    eligibility['reasons'] = []  # list of all the reasons that contribute towards uneligibilty
+	eligibility = {}
+	eligibility['value'] = True
+	eligibility['reasons'] = []  # list of all the reasons that contribute towards uneligibilty
 
-    if (candidate.batch.pg_or_not == 'G'):
-        if (candidate.cgpa_ug < job.cgpa_min):
-            eligibility['value'] = False
-            eligibility['reasons'].append("Your CGPA is below the requirement.")
-    else:
-        if (candidate.cgpa_pg < job.cgpa_min):
-            eligibility['value'] = False
-            eligibility['reasons'].append("Your CGPA is below the requirement.")
+	if (candidate.batch.pg_or_not == 'G'):
+		if (candidate.cgpa_ug < job.cgpa_min):
+			eligibility['value'] = False
+			eligibility['reasons'].append("Your CGPA is below the requirement.")
+	else:
+		if (candidate.cgpa_pg < job.cgpa_min):
+			eligibility['value'] = False
+			eligibility['reasons'].append("Your CGPA is below the requirement.")
 
-    if (candidate.percentage_tenth < job.min_tenthmarks):
-        eligibility['value'] = False
-        eligibility['reasons'].append("Your 10th Marks are below the requirement.")
+	if (candidate.percentage_tenth < job.min_tenthmarks):
+		eligibility['value'] = False
+		eligibility['reasons'].append("Your 10th Marks are below the requirement.")
 
-    if (candidate.percentage_twelfth < job.min_twelfthmarks):
-        eligibility['value'] = False
-        eligibility['reasons'].append("Your 12th Marks are below the requirement.")
+	if (candidate.percentage_twelfth < job.min_twelfthmarks):
+		eligibility['value'] = False
+		eligibility['reasons'].append("Your 12th Marks are below the requirement.")
 
-    if (candidate.backlogs > job.max_blacklogs):
-        eligibility['value'] = False
-        eligibility['reasons'].append("You have too many backlogs.")
+	if (candidate.backlogs > job.max_blacklogs):
+		eligibility['value'] = False
+		eligibility['reasons'].append("You have too many backlogs.")
 
-    if (candidate.status == 'B'):
-        eligibility['value'] = False
-        eligibility['reasons'].append("You have been blocked by the placement cell.")
+	if (candidate.status == 'B'):
+		eligibility['value'] = False
+		eligibility['reasons'].append("You have been blocked by the placement cell.")
 
-    if (job.status == 'C' or job.status == 'A'):
-        eligibility['value'] = False
-        eligibility['reasons'].append("This Job cannot be applied to.")
+	if (job.status == 'C' or job.status == 'A'):
+		eligibility['value'] = False
+		eligibility['reasons'].append("This Job cannot be applied to.")
 
-    Vals = []
-    for b in job.batch.all():
-        if (b != candidate.batch):
-            Vals.append(False)
-        else:
-            Vals.append(True)
-    if not any(Vals):
-        eligibility['value'] = False
-        eligibility['reasons'].append("You are not eligible for this job!")
-    return eligibility
+	Vals = []
+	for b in job.batch.all():
+		if (b != candidate.batch):
+			Vals.append(False)
+		else:
+			Vals.append(True)
+	if not any(Vals):
+		eligibility['value'] = False
+		eligibility['reasons'].append("You are not eligible for this job!")
+	return eligibility
 
 
 def checkdeadline(job):
-    if (timezone.now() > job.deadline):
-        return True
-    else:
-        return False
+	if (timezone.now() > job.deadline):
+		return True
+	else:
+		return False
 
 
 def is_admin(user):
-    allowed_group = {'admin'}
-    usr = User.objects.get(username=user)
-    groups = [x.name for x in usr.groups.all()]
-    if allowed_group.intersection(set(groups)):
-        return True
-    return False
+	allowed_group = {'admin'}
+	usr = User.objects.get(username=user)
+	groups = [x.name for x in usr.groups.all()]
+	if allowed_group.intersection(set(groups)):
+		return True
+	return False
 
 
 def special_match(strg, search=re.compile(r'[^A-Za-z0-9., -]').search):
-    return not bool(search(strg))
+	return not bool(search(strg))
 
 
 def contact_match(strg, search=re.compile(r'[0-9]\n').search):
-    return not bool(search(strg))
+	return not bool(search(strg))
 
 
 def onlyspecchar_match(strg, search=re.compile(r'^[., -]').search):
-    return not bool(search(strg))
+	return not bool(search(strg))
 
 
 def onlynumbers(strg, search=re.compile(r'^[0-9]').search):
-    return bool(search(strg))
+	return bool(search(strg))
--- placement/jobport/middleware.py
+++ placement/jobport/middleware.py
@@ -1,37 +1,8 @@
 from social.apps.django_app.middleware import SocialAuthExceptionMiddleware
 from django.shortcuts import render
 from social.exceptions import AuthForbidden
-
-#class CvControlMiddleware(object):
- #   def process_request(self,request):
-  #      current_url = request.get_full_path()
-   #     geturl = urlparse(current_url)
-    #    name = request.user.username.split('@')[0].lower()
-     #   if name != geturl.path.split('/')[-1].split('.')[0].split('_')[0].lower():
-      #      return render(request, "jobport/notallowed.html")
-
     
 class MyMiddleware(SocialAuthExceptionMiddleware):
     def process_exception(self, request, exception):
         if type(exception) == AuthForbidden:
             return render(request, "jobport/needlogin.html", {})
-
-#    def process_request(self,request):
-#        if request.user.is_authenticated():
-#            name = request.user.username.split('@')[0]
-#            roll=[]
-#            for i in name:
-#                if i.isdigit():
-#                    roll.append(i)
-#
-#            if len(roll) == 5:
-#                if int(roll[1])>1 or int(roll[1])<1:
-#                    auth_logout(request)
-#                    return render(request,"jobport/notlogin.html",{})
-#            elif len(roll) == 4:
-#                if int(roll[1])>3 or int(roll[1])<3:
-#                    auth_logout(request)
-#                    return render(request,"jobport/notlogin.html",{})
-
-#    def process_request(self,request):
-        
