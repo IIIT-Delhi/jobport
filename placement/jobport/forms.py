d1a9aec2cc2dd3458459c4c50429eec8cca54904
naman <namanspace@gmail.com>
2015-09-05 19:02:40 +0530

    added licence

--- placement/jobport/forms.py
+++ placement/jobport/forms.py
@@ -1,3 +1,13 @@
+# //=======================================================================
+# // Copyright JobPort, IIIT Delhi 2015.
+# // Distributed under the MIT License.
+# // (See accompanying file LICENSE or copy at
+# //  http://opensource.org/licenses/MIT)
+# //=======================================================================
+
+
+
+
 # __author__ = 'naman'
 
 import re
--- placement/jobport/storage.py
+++ placement/jobport/storage.py
@@ -1,3 +1,15 @@
+# //=======================================================================
+# // Copyright JobPort, IIIT Delhi 2015.
+# // Distributed under the MIT License.
+# // (See accompanying file LICENSE or copy at
+# //  http://opensource.org/licenses/MIT)
+# //=======================================================================
+
+
+
+
+# __author__ = 'naman'
+
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 import os
